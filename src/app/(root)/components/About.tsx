2f5235855deff91cd2f0bd1dade1eb145ad054ab
MEGARAJAN-S <megarajanmegan@gmail.com>
2025-07-13 22:18:25 +0530

    04: Value Section and Small features

--- src/app/(root)/components/About.tsx
+++ src/app/(root)/components/About.tsx
@@ -5,9 +5,15 @@ import { features } from '@/constants/features'
 const About = () => {
     return (
         <div>
-            <div className='w-full h-[50rem] pt-20'>
-                <div className='w-full h-[30rem] bg-black'>
-                    <div className='flex items-center justify-around pt-20'>
+            <div className='h-svh bg-black items-center justify-center rounded-t-full border-t-2 border-white'>
+                <div className='absolute w-full bg-linear-to-t from-background to-background h-svh z-[-1]'>
+
+                </div>
+                <div>
+                    <div className='flex items-center justify-center py-50'>
+                        <span className='text-2xl sm:text-3xl md:text-4xl lg:text-6xl'>Powerful Features for your Gym Growth</span>
+                    </div>
+                    <div className='flex items-center justify-around'>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
                             {features.map((feature, index) => (
                                 <Card
